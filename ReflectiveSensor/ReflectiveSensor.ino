095a63d6f7f96808c58b3d88559da415c2cba78d
Per Lenander <per.lenander@addiva.se>
2023-03-16 19:04:42 +0100

    Comments

--- ReflectiveSensor/ReflectiveSensor.ino
+++ ReflectiveSensor/ReflectiveSensor.ino
@@ -1,14 +1,17 @@
 #include <ReflectiveSensor.h>
 
+// Parameter is pin number sensor is connected on
 ReflectiveSensor sensor(3);
 
 void setup() {
-  // put your setup code here, to run once:
+  // Open up a serial console
   Serial.begin(9600);
 }
 
 void loop() {
+  // Read sensor value
   int retval = sensor.get_reading();
+  // Print out sensor value as a decimal number
   Serial.print(retval, DEC);
   Serial.print("\n");
 }
